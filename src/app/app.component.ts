a7b13b109fbec10a329115fadc03f5ccbcc865ed
Alison Killen <61318985+alikillen@users.noreply.github.com>
2022-12-30 15:21:51 +1000

    first commit - project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+// component declaration. selector is html tag you use to embed your component. templateurl is the HTML file we use. styleurls (can have more than 1) is css.
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// classes - put any properties of the component here, and any methods, custom and lifecycle methods (kinda like react class components)
+// lets define title as a string with TS
 export class AppComponent {
-  title = 'angular-crash-course';
+  title: string = 'Task Tracker';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+// when you create a component, like app component, our CLI provides it automatically - when we use a component we need to provide declarations, imports, providers, bootstrap (?)
+// generally components will have 4 files - typescript compoennt file with main properties methods etc, template html file, css file, spec file for testing 
 @NgModule({
   declarations: [
     AppComponent
